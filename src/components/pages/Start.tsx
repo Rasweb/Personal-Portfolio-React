a10ff0717d5d6d6bbd78e6765b4dba5b0e1fecb8
Rasweb <Rasmuspalm@fastmail.se>
2022-09-05 14:28:06 +0200

    Start page html structure

--- src/components/pages/Start.tsx
+++ src/components/pages/Start.tsx
@@ -2,7 +2,20 @@ export const Start = () => {
   return (
     <>
       <div>
-        <h1>This is start</h1>
+        <h1>Hello, my name is Rasmus</h1>
+        <p>Welcome to my personal website!</p>
+        <p>
+          I'm a front end developer student at Medieinstitutet Stockholm,
+          Sweden. On this site you can find all my public projects and my social
+          links.
+        </p>
+        <p>
+          I have done projects using Bootstrap, Css, Cypress, Express, Html,
+          JavaScript, NodeJS, React, Scss/sass, Tailwind, TypeScript, and Vue.
+        </p>
+      </div>
+      <div>
+        <a href="https://github.com/Rasweb">Github</a>
       </div>
     </>
   );
