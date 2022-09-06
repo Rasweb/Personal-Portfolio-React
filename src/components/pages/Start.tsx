478ae2df1716b7650fc2411e3841471a7c76d5b3
Rasweb <Rasmuspalm@fastmail.se>
2022-09-06 10:44:23 +0200

    start page text styling

--- src/components/pages/Start.tsx
+++ src/components/pages/Start.tsx
@@ -1,21 +1,25 @@
 export const Start = () => {
   return (
     <>
-      <div>
-        <h1>Hello, my name is Rasmus</h1>
-        <p>Welcome to my personal website!</p>
-        <p>
-          I'm a front end developer student at Medieinstitutet Stockholm,
-          Sweden. On this site you can find all my public projects and my social
-          links.
-        </p>
-        <p>
-          I have done projects using Bootstrap, Css, Cypress, Express, Html,
-          JavaScript, NodeJS, React, Scss/sass, Tailwind, TypeScript, and Vue.
-        </p>
-      </div>
-      <div>
-        <a href="https://github.com/Rasweb">Github</a>
+      <div className="flex flex-col items-center">
+        <div className="text-3xl ">
+          <h1>Hello, my name is Rasmus</h1>
+        </div>
+        <div>
+          <p className="text-lg font-serif">
+            I'm a front end developer student at Medieinstitutet Stockholm,
+            Sweden. On this site you can find all my public projects.
+          </p>
+          <p className="text-lg font-serif">
+            I have worked with projects using Bootstrap, CSS, Cypress, Express,
+            HTML, JavaScript, NodeJS, React, Scss/sass, Tailwind, TypeScript,
+            and Vue.
+          </p>
+          <p></p>
+        </div>
+        <div>
+          <a href="https://github.com/Rasweb">Github</a>
+        </div>
       </div>
     </>
   );
